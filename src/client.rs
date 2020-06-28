969cf776394afe3627e4ab3a3384ffa692a058be
Vishwa Patel <vpatel@mozilla.com>
2020-06-28 14:39:06 -0400

    Enable records filtering

--- src/client.rs
+++ src/client.rs
@@ -3,6 +3,8 @@ mod signatures;
 
 use kinto_http::{get_changeset, KintoObject, KintoError};
 use signatures::{DefaultVerifier};
+use std::collections::HashMap;
+use serde_json::Value;
 pub use signatures::{SignatureError, Verification};
 
 pub const DEFAULT_SERVER_URL: &str = "https://firefox.settings.services.mozilla.com/v1";
@@ -111,8 +113,72 @@ impl Client {
         }
     }
 
+    fn matches(record: &KintoObject, filters: &HashMap<&str, serde_json::Value>) -> bool {
+
+        for (key, value) in filters {
+            let org_value = match (*record).get(key) {
+                Some(value) => value,
+                None => return false
+            };
+
+            match(org_value, value) {
+                (Value::Null, Value::Null) => continue,
+                (Value::Bool(b1), Value::Bool(b2)) => {
+                    if b1 == b2 { continue } else { return false }
+                },
+                (Value::Number(number1), Value::Number(number2)) => {
+                    if number1 == number2 { continue } else { return false }
+                },
+                (Value::String(string1), Value::String(string2)) => {
+                    if string1 == string2 { continue } else { return false }
+                },
+                (Value::Array(vector1), Value::Array(vector2)) => {
+                    if vector1 == vector2 { continue } else { return false }
+                },
+                (Value::Object(hashmap1), Value::Object(hashmap2)) => {
+                    if hashmap1 == hashmap2 { continue } else { return false }
+                },
+                _ => return false
+            };
+        }
+
+        return true;
+    }
+
+    pub async fn get(&self, expected: u64, filters: std::collections::HashMap<&str, serde_json::value::Value>) -> Result<Collection, ClientError> {
+        let changeset = get_changeset(
+            &self.server_url,
+            &self.bucket_name,
+            &self.collection_name,
+            expected,
+       )
+       .await?;
+
+       println!("changeset.metadata {}", serde_json::to_string_pretty(&changeset.metadata)?);
+
+        // filter collection records
+       let mut filtered_records = Vec::new();
+
+       for record in changeset.changes {
+           if Client::matches(&record, &filters) {
+               filtered_records.push(record);
+           }
+       }
+
+       let collection = Collection {
+           bid: self.bucket_name.to_string(),
+           cid: self.collection_name.to_string(),
+           metadata: changeset.metadata,
+           records: filtered_records,
+           timestamp: changeset.timestamp
+        };
+
+        self.verifier.verify(&collection).await?;
+        Ok(collection)
+    }
+    
     // For parameter expected, default value is 0
-    pub async fn get(&self, expected: u64) -> Result<Collection, ClientError> {
+    pub async fn get_all(&self, expected: u64) -> Result<Collection, ClientError> {
         let changeset = get_changeset(
             &self.server_url,
             &self.bucket_name,
@@ -145,6 +211,7 @@ mod tests {
     use async_trait::async_trait;
     use super::signatures::{Verification, SignatureError};
     use super::{Client, Collection, ClientError};
+    use std::collections::HashMap;
 
     struct VerifierWithVerificatonError {}
     struct VerifierWithNoError {}
@@ -186,7 +253,7 @@ mod tests {
             }"#
         ).create();
 
-        let actual_result = Client::create_with_server_collection("http://localhost:5000", "url-classifier-skip-urls", Some(Box::new(VerifierWithVerificatonError{}))).get(0).await;
+        let actual_result = Client::create_with_server_collection("http://localhost:5000", "url-classifier-skip-urls", Some(Box::new(VerifierWithVerificatonError{}))).get_all(0).await;
         let expected_result = Err(ClientError::VerificationError{name: "verification error".to_owned()});
         assert_eq!(1, get_changeset_mock.times_called());
         assert_eq!(expected_result, actual_result);
@@ -214,7 +281,7 @@ mod tests {
             }"#
         ).create();
 
-        let actual_result = Client::create_with_server_collection("http://localhost:5000", "url-classifier-skip-urls", Some(Box::new(VerifierWithNoError{}))).get(expected_version).await;
+        let actual_result = Client::create_with_server_collection("http://localhost:5000", "url-classifier-skip-urls", Some(Box::new(VerifierWithNoError{}))).get_all(expected_version).await;
         assert_eq!(1 , get_changeset_mock.times_called());
 
         match actual_result {
@@ -255,9 +322,400 @@ mod tests {
             }"#
         ).create();
 
-        let actual_result = Client::create_with_server_collection("http://localhost:5000", "url-classifier-skip-urls", Some(Box::new(VerifierWithInvalidSignatureError{}))).get(0).await;
+        let actual_result = Client::create_with_server_collection("http://localhost:5000", "url-classifier-skip-urls", Some(Box::new(VerifierWithInvalidSignatureError{}))).get_all(0).await;
         let expected_result = Err(ClientError::Error{name: "invalid signature error".to_owned()});
         assert_eq!(expected_result, actual_result);
         assert_eq!(1, get_changeset_mock.times_called());
+    }
+
+    #[test]
+    fn test_matches_returns_false_mismatching_object_and_null_value_types() {
+
+        let record = json!({"id": json!({"nested": 2})});
+        let mut filters = HashMap::new();
+        filters.insert("id", json!(null));
+
+        assert_eq!(Client::matches(&record, &filters), false);
+    }
+    
+    #[test]
+    fn test_matches_returns_false_mismatching_bool_and_null_value_types() {
+
+        let record = json!(true);
+        let mut filters = HashMap::new();
+        filters.insert("id", json!(null));
+
+        assert_eq!(Client::matches(&record, &filters), false);
+    }
+
+    #[test]
+    fn test_matches_returns_false_mismatching_number_and_null_value_types() {
+
+        let record = json!(12);
+        let mut filters = HashMap::new();
+        filters.insert("id", json!(null));
+
+        assert_eq!(Client::matches(&record, &filters), false);
+    }
+
+    #[test]
+    fn test_matches_returns_false_mismatching_string_and_null_value_types() {
+
+        let record = json!("does not match");
+        let mut filters = HashMap::new();
+        filters.insert("id", json!(null));
+
+        assert_eq!(Client::matches(&record, &filters), false);
+    }
+
+    #[test]
+    fn test_matches_returns_false_mismatching_array_and_null_value_types() {
+
+        let record = json!(vec![1,2,3]);
+        let mut filters = HashMap::new();
+        filters.insert("id", json!(null));
+
+        assert_eq!(Client::matches(&record, &filters), false);
+    }
+
+    #[test]
+    fn test_matches_returns_false_mismatching_bool_and_array_value_types() {
+
+        let record = json!(vec![1,2,3]);
+        let mut filters = HashMap::new();
+        filters.insert("id", json!(true));
+
+        assert_eq!(Client::matches(&record, &filters), false);
+    }
+
+    #[test]
+    fn test_matches_returns_false_mismatching_bool_and_number_value_types() {
+
+        let record = json!(123);
+        let mut filters = HashMap::new();
+        filters.insert("id", json!(null));
+
+        assert_eq!(Client::matches(&record, &filters), false);
+    }
+
+    #[test]
+    fn test_matches_returns_false_mismatching_bool_and_string_value_types() {
+
+        let record = json!("does not match");
+        let mut filters = HashMap::new();
+        filters.insert("id", json!(null));
+
+        assert_eq!(Client::matches(&record, &filters), false);
+    }
+
+    #[test]
+    fn test_matches_returns_false_mismatching_bool_and_object_value_types() {
+
+        let record = json!({"id": 2});
+        let mut filters = HashMap::new();
+        filters.insert("id", json!(null));
+
+        assert_eq!(Client::matches(&record, &filters), false);
+    }
+
+    #[test]
+    fn test_matches_returns_false_mismatching_number_and_string_value_types() {
+
+        let record = json!("does not match");
+        let mut filters = HashMap::new();
+        filters.insert("id", json!(123));
+
+        assert_eq!(Client::matches(&record, &filters), false);
+    }
+
+    #[test]
+    fn test_matches_returns_false_mismatching_number_and_array_value_types() {
+
+        let record = json!(vec![1,2,3]);
+        let mut filters = HashMap::new();
+        filters.insert("id", json!(123));
+
+        assert_eq!(Client::matches(&record, &filters), false);
+    }
+
+    #[test]
+    fn test_matches_returns_false_mismatching_number_and_object_value_types() {
+
+        let record = json!({"id": 1});
+        let mut filters = HashMap::new();
+        filters.insert("id", json!(123));
+
+        assert_eq!(Client::matches(&record, &filters), false);
+    }
+
+    #[test]
+    fn test_matches_returns_false_mismatching_string_and_array_value_types() {
+
+        let record = json!(vec!["does not match", "matches", "I don't know"]);
+        let mut filters = HashMap::new();
+        filters.insert("id", json!("does not match"));
+
+        assert_eq!(Client::matches(&record, &filters), false);
+    }
+
+    #[test]
+    fn test_matches_returns_false_mismatching_string_and_object_value_types() {
+
+        let record = json!({"matches": "does not match"});
+        let mut filters = HashMap::new();
+        filters.insert("id", json!("does not match"));
+
+        assert_eq!(Client::matches(&record, &filters), false);
+    }
+
+    #[test]
+    fn test_matches_returns_false_mismatching_object_and_array_value_types() {
+
+        let record = json!({"id": 1, "matches": false});
+        let mut filters = HashMap::new();
+        filters.insert("id", json!(vec![1,2,3]));
+
+        assert_eq!(Client::matches(&record, &filters), false);
+    }
+
+    #[test]
+    fn test_matches_correctly_matches_null_values() {
+
+        let record = json!({"id": json!(null), "nested": {"id": 2, "name": "vishwa"}});
+        let mut filters = HashMap::new();
+        filters.insert("id", json!(null));
+        filters.insert("nested", json!({"id": 2, "name": "vishwa"}));
+
+        assert_eq!(Client::matches(&record, &filters), true);
+    }
+
+    #[test]
+    fn test_matches_correctly_matches_bool_values() {
+
+        let record = json!({"matches": true});
+        let mut filters = HashMap::new();
+        filters.insert("matches", json!(true));
+
+        assert_eq!(Client::matches(&record, &filters), true);
+    }
+
+    #[test]
+    fn test_matches_correctly_matches_number_values() {
+
+        let record = json!({"number": json!(123)});
+        let mut filters = HashMap::new();
+        filters.insert("number", json!(123));
+
+        assert_eq!(Client::matches(&record, &filters), true);
+    }
+
+    #[test]
+    fn test_matches_correctly_matches_string_values() {
+
+        let record = json!({"string": json!("test")});
+        let mut filters = HashMap::new();
+        filters.insert("string", json!("test"));
+
+        assert_eq!(Client::matches(&record, &filters), true);
+    }
+
+    #[test]
+    fn test_matches_correctly_matches_array_values() {
+
+        let record = json!({"array": json!(vec!["one","two","three"])});
+        let mut filters = HashMap::new();
+        filters.insert("array", json!(vec!["one","two","three"]));
+
+        assert_eq!(Client::matches(&record, &filters), true);
+    }
+
+    #[test]
+    fn test_matches_correctly_matches_object_values() {
+
+        let record = json!({"object": json!({"id": 1, "nested_object": json!({"id": 2})})});
+        let mut filters = HashMap::new();
+        filters.insert("object", json!({"id": 1, "nested_object": json!({"id": 2})}));
+
+        assert_eq!(Client::matches(&record, &filters), true);
+    }
+
+    #[test]
+    fn test_matches_correctly_matches_array_of_object_values() {
+
+        let record = json!({"array": json!(vec![json!({"id": 1}), json!({"id": 2})])});
+        let mut filters = HashMap::new();
+        filters.insert("array", json!(vec![json!({"id": 1}), json!({"id": 2})]));
+
+        assert_eq!(Client::matches(&record, &filters), true);
+    }
+
+    #[tokio::test]
+    #[with_mock_server]
+    async fn test_get_correctly_filters_records() {
+
+        let get_changeset_mock = mock(GET, "/buckets/main/collections/url-classifier-skip-urls/changeset")
+        .expect_query_param("_expected", "0")
+        .return_status(200)
+        .return_header("Content-Type", "application/json")
+        .return_body(
+            r#"{
+                "metadata": {"data": "test"},
+                "changes": [
+                    {
+                        "id": 1,
+                        "last_modified": 100
+                    },
+                    {
+                        "id": 2,
+                        "last_modified": 200
+                    }
+                ],
+                "timestamp": 0
+            }"#
+        ).create();
+
+        let mut filters: HashMap<&str, serde_json::value::Value> = HashMap::new();
+        filters.insert("id", json!(2));
+        filters.insert("last_modified", json!(200));
+
+        let expected_version: u64 = 0;
+    
+        let actual_result = Client::create_with_server_collection("http://localhost:5000", "url-classifier-skip-urls", Some(Box::new(VerifierWithNoError{})))
+                                    .get(expected_version, filters).await;
+
+        assert_eq!(1, get_changeset_mock.times_called());
+        
+        match actual_result {
+            Ok(collection) => {
+                assert_eq!(
+                    Collection {
+                        bid: "main".to_owned(),
+                        cid: "url-classifier-skip-urls".to_owned(),
+                        metadata: json!({
+                            "data": "test".to_owned()
+                        }),
+                        records: vec![
+                            json!({
+                                "id": 2,
+                                "last_modified": 200
+                            })
+                        ],
+                        timestamp: 0
+                    }, collection
+                )
+            },
+            Err(error) => panic!("invalid response : {:?}", error)
+        };
+    }
+
+    #[tokio::test]
+    #[with_mock_server]
+    async fn test_get_correctly_returns_all_records_when_no_filters_present() {
+        let get_changeset_mock = mock(GET, "/buckets/main/collections/url-classifier-skip-urls/changeset")
+        .expect_query_param("_expected", "0")
+        .return_status(200)
+        .return_header("Content-Type", "application/json")
+        .return_body(
+            r#"{
+                "metadata": {"data": "test"},
+                "changes": [
+                    {
+                        "id": 1,
+                        "last_modified": 100
+                    },
+                    {
+                        "id": 2,
+                        "last_modified": 200
+                    }
+                ],
+                "timestamp": 0
+            }"#
+        ).create();
+
+        let expected_version: u64 = 0;
+    
+        let actual_result = Client::create_with_server_collection("http://localhost:5000", "url-classifier-skip-urls", Some(Box::new(VerifierWithNoError{})))
+                                    .get(expected_version, HashMap::new()).await;
+
+        assert_eq!(1, get_changeset_mock.times_called());
+        
+        match actual_result {
+            Ok(collection) => {
+                assert_eq!(
+                    Collection {
+                        bid: "main".to_owned(),
+                        cid: "url-classifier-skip-urls".to_owned(),
+                        metadata: json!({
+                            "data": "test".to_owned()
+                        }),
+                        records: vec![
+                            json!({
+                                "id": 1,
+                                "last_modified": 100
+                            }),
+                            json!({
+                                "id": 2,
+                                "last_modified": 200
+                            })
+                        ],
+                        timestamp: 0
+                    }, collection
+                )
+            },
+            Err(error) => panic!("invalid response : {:?}", error)
+        };
+    }
+
+    #[tokio::test]
+    #[with_mock_server]
+    async fn test_get_correctly_returns_no_records_when_no_record_matches_filters() {
+        let get_changeset_mock = mock(GET, "/buckets/main/collections/url-classifier-skip-urls/changeset")
+        .expect_query_param("_expected", "0")
+        .return_status(200)
+        .return_header("Content-Type", "application/json")
+        .return_body(
+            r#"{
+                "metadata": {"data": "test"},
+                "changes": [
+                    {
+                        "id": 1,
+                        "last_modified": 100
+                    },
+                    {
+                        "id": 2,
+                        "last_modified": 200
+                    }
+                ],
+                "timestamp": 0
+            }"#
+        ).create();
+
+        let mut filters: HashMap<&str, serde_json::Value> = HashMap::new();
+
+        filters.insert("id", json!(3));
+
+        let expected_version: u64 = 0;
+    
+        let actual_result = Client::create_with_server_collection("http://localhost:5000", "url-classifier-skip-urls", Some(Box::new(VerifierWithNoError{})))
+                                    .get(expected_version, filters).await;
+
+        assert_eq!(1, get_changeset_mock.times_called());
+        
+        match actual_result {
+            Ok(collection) => {
+                assert_eq!(
+                    Collection {
+                        bid: "main".to_owned(),
+                        cid: "url-classifier-skip-urls".to_owned(),
+                        metadata: json!({
+                            "data": "test".to_owned()
+                        }),
+                        records: vec![],
+                        timestamp: 0
+                    }, collection
+                )
+            },
+            Err(error) => panic!("invalid response : {:?}", error)
+        };
     }
 }
