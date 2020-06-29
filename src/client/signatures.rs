0f9940cc4eb88d649121c5bac9caee73781a4c29
Vishwa Patel <vpatel@mozilla.com>
2020-06-29 15:11:50 -0400

    Add Signature Verifier as a dependency to the Remote Settings Client

--- src/client/signatures.rs
+++ src/client/signatures.rs
@@ -1,11 +1,26 @@
 use crate::client::Collection;
 
+mod canonical_json;
+
 use async_trait::async_trait;
+use canonical_json::serialize;
+use openssl::bn::BigNumContext;
+use openssl::ec::{EcGroup, PointConversionForm};
+use openssl::nid::Nid;
+use openssl::x509::X509;
+use base64;
+use reqwest;
+use serde_json::json;
+use signatory::{
+    ecdsa::{curve::NistP384, FixedSignature},
+    verify_sha384, EcdsaPublicKey, Signature,
+};
+use signatory_ring::ecdsa::P384Verifier;
 use reqwest::Error as ReqwestError;
 
 #[async_trait]
 pub trait Verification {
-    async fn verify(collection: &Collection) -> Result<(), SignatureError>;
+    async fn verify(&self, collection: &Collection) -> Result<(), SignatureError>;
 }
 
 #[derive(Debug)]
@@ -39,5 +54,51 @@ impl From<base64::DecodeError> for SignatureError {
         SignatureError::InvalidSignature {
             name: err.to_string(),
         }
+    }
+}
+
+pub struct DefaultVerifier {}
+
+#[async_trait]
+impl Verification for DefaultVerifier {
+    async fn verify(&self, collection: &Collection) -> Result<(), SignatureError> {
+        // Fetch certificate PEM (public key).
+        let x5u = collection.metadata["signature"]["x5u"].as_str().unwrap();
+        let resp = reqwest::get(&x5u.to_string()).await?;
+        let pem = resp.bytes().await?;
+
+        // Parse PEM (OpenSSL)
+        let cert = X509::from_pem(&pem)?;
+        let public_key = cert.public_key()?;
+        let ec_public_key = public_key.ec_key()?;
+        let mut ctx = BigNumContext::new()?;
+        let group = EcGroup::from_curve_name(Nid::SECP384R1)?;
+        let public_key_bytes = ec_public_key.public_key().to_bytes(
+            &group,
+            PointConversionForm::UNCOMPRESSED,
+            &mut ctx,
+        )?;
+        let pk: EcdsaPublicKey<NistP384> = EcdsaPublicKey::from_bytes(&public_key_bytes)?;
+
+        // Instantiate signature
+        let b64_signature = collection.metadata["signature"]["signature"]
+            .as_str()
+            .unwrap_or("");
+        let signature_bytes = base64::decode_config(&b64_signature, base64::URL_SAFE)?;
+        let signature = FixedSignature::<NistP384>::from_bytes(&signature_bytes)?;
+
+        // Serialized data.
+        let mut sorted_records = collection.records.to_vec();
+        sorted_records.sort_by(|a, b| (a["id"]).to_string().cmp(&b["id"].to_string()));
+        let serialized = serialize(&json!({
+            "data": sorted_records,
+            "last_modified": collection.timestamp.to_string().to_owned()
+        }));
+        let data = format!("Content-Signature:\x00{}", serialized);
+
+        // Verify
+        verify_sha384(&P384Verifier::from(&pk), &data.as_bytes(), &signature)?;
+
+        Ok(())
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,9 @@
 pub mod client;
+
+pub use client::Collection;
+pub use client::Client;
+pub use client::Verification;
+pub use client::SignatureError;
+
+pub use client::DEFAULT_BUCKET_NAME;
+pub use client::DEFAULT_SERVER_URL;
