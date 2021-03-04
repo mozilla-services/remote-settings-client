93042f5630b2e02f8603f92e0c2f3bfa1053004f
Mathieu Leplatre <mathieu@mozilla.com>
2021-03-04 14:57:49 +0100

    Reformat demo code

--- rs-client-demo/src/main.rs
+++ rs-client-demo/src/main.rs
@@ -3,26 +3,23 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 use env_logger;
 use remote_settings_client::{Client, Collection, SignatureError, Verification};
-use viaduct::{set_backend, Request};
-pub use url::{ParseError, Url};
-pub use viaduct_reqwest::ReqwestBackend;
-use serde_json;
 use serde::Deserialize;
+use serde_json;
+pub use url::{ParseError, Url};
+use viaduct::{set_backend, Request};
+pub use viaduct_reqwest::ReqwestBackend;
 
 struct CustomVerifier {}
 
 #[derive(Deserialize, Debug)]
 struct KintoPluralResponse<T> {
-    data: Vec<T>,
+    changes: Vec<T>,
 }
 
 #[derive(Deserialize, Debug)]
-pub struct LatestChangeResponse {
-    pub id: String,
-    pub last_modified: u64,
+pub struct LatestChangeEntry {
     pub bucket: String,
     pub collection: String,
-    pub host: String,
 }
 
 impl Verification for CustomVerifier {
@@ -43,34 +40,36 @@ fn main() {
 
     println!("Fetching records using RS client with default Verifier");
 
-    let client = Client::builder().collection_name("url-classifier-skip-urls").build();
+    let client = Client::builder()
+        .collection_name("url-classifier-skip-urls")
+        .build();
 
     match client.get() {
-        Ok(records) => {
-            print_records(&records)
-        },
+        Ok(records) => print_records(&records),
         Err(error) => println!("Error fetching/verifying records: {:?}", error),
     };
 
-    let url = "https://firefox.settings.services.mozilla.com/v1/buckets/monitor/collections/changes/records";
+    let url = "https://firefox.settings.services.mozilla.com/v1/buckets/monitor/collections/changes/changeset?_expected=0";
     let response = Request::get(Url::parse(&url).unwrap()).send().unwrap();
-    let collections: KintoPluralResponse<LatestChangeResponse> = response.json().unwrap(); 
+    let collections: KintoPluralResponse<LatestChangeEntry> = response.json().unwrap();
 
     let mut failed_fetch = Vec::new();
 
-    for collection in &collections.data {
-        
-        let client = Client::builder().bucket_name(&collection.bucket).collection_name(&collection.collection).build();
+    for collection in &collections.changes {
+        let client = Client::builder()
+            .bucket_name(&collection.bucket)
+            .collection_name(&collection.collection)
+            .build();
 
         println!("Fetching records");
 
-       match client.get() {
-           Ok(records) => print_records(&records),
-           Err(error) => {
-               println!("Error fetching/verifying records: {:?}", error);
-               failed_fetch.push(collection);
-           },
-       };
+        match client.get() {
+            Ok(records) => print_records(&records),
+            Err(error) => {
+                println!("Error fetching/verifying records: {:?}", error);
+                failed_fetch.push(collection);
+            }
+        };
 
         println!(" -- --- -- -- -- -- - -");
     }
