34fb95b665f5eecd07213ea1b72ccef99226b325
Mathieu Leplatre <mathieu@mozilla.com>
2021-03-08 13:41:06 +0100

    Improve demo client

--- rs-client-demo/src/main.rs
+++ rs-client-demo/src/main.rs
@@ -4,7 +4,6 @@
 use env_logger;
 use remote_settings_client::{Client, Collection, SignatureError, Verification};
 use serde::Deserialize;
-use serde_json;
 pub use url::{ParseError, Url};
 use viaduct::{set_backend, Request};
 pub use viaduct_reqwest::ReqwestBackend;
@@ -28,25 +27,31 @@ impl Verification for CustomVerifier {
     }
 }
 
-fn print_records(records: &[serde_json::Value]) {
-    for record in records {
-        println!("{:?}", record);
-    }
-}
-
 fn main() {
     env_logger::init();
     set_backend(&ReqwestBackend).unwrap();
 
-    println!("Fetching records using RS client with default Verifier");
+    print!("Fetching records using RS client with default Verifier");
 
     let client = Client::builder()
         .collection_name("url-classifier-skip-urls")
         .build();
 
     match client.get() {
-        Ok(records) => print_records(&records),
-        Err(error) => println!("Error fetching/verifying records: {:?}", error),
+        Ok(records) => println!("{} records.", records.len()),
+        Err(error) => println!("FAILED ({:?})", error),
+    };
+
+    print!("Fetching records using RS client with custom Verifier");
+
+    let client_with_custom_verifier = Client::builder()
+        .collection_name("url-classifier-skip-urls")
+        .verifier(Box::new(CustomVerifier {}))
+        .build();
+
+    match client_with_custom_verifier.get() {
+        Ok(records) => println!("{} records.", records.len()),
+        Err(error) => println!("FAILED ({:?})", error),
     };
 
     let url = "https://firefox.settings.services.mozilla.com/v1/buckets/monitor/collections/changes/changeset?_expected=0";
@@ -56,34 +61,22 @@ fn main() {
     let mut failed_fetch = Vec::new();
 
     for collection in &collections.changes {
+        let cid = format!("{}/{}", collection.bucket, collection.collection);
+        print!("Fetching records of {}: ", cid);
+
         let client = Client::builder()
             .bucket_name(&collection.bucket)
             .collection_name(&collection.collection)
             .build();
 
-        println!("Fetching records");
-
         match client.get() {
-            Ok(records) => print_records(&records),
+            Ok(records) => println!("{} records.", records.len()),
             Err(error) => {
-                println!("Error fetching/verifying records: {:?}", error);
-                failed_fetch.push(collection);
+                println!("FAILED ({:?})", error);
+                failed_fetch.push(cid);
             }
         };
-
-        println!(" -- --- -- -- -- -- - -");
     }
 
     println!("The failed collections are {:?}", failed_fetch);
-
-    println!("Fetching records using RS client with custom Verifier");
-    let client_with_custom_verifier = Client::builder()
-        .collection_name("url-classifier-skip-urls")
-        .verifier(Box::new(CustomVerifier {}))
-        .build();
-
-    match client_with_custom_verifier.get() {
-        Ok(records) => print_records(&records),
-        Err(error) => println!("Error fetching/verifying records: {:?}", error),
-    };
 }
