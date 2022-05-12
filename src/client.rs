76089247a22a42b9f6ff98916fbb94e1150d2f13
Mathieu Leplatre <mathieu@mozilla.com>
2022-05-12 23:04:28 +0200

    Update mentions of A-S versions

--- src/client.rs
+++ src/client.rs
@@ -272,7 +272,7 @@ pub struct Collection {
 ///
 /// ### `rc_crypto`
 ///
-/// With the `rc_crypto` feature, a signature verifier leveraging the [`rc_crypto` crate](https://github.com/mozilla/application-services/tree/v91.1.0/components/support/rc_crypto).
+/// With the `rc_crypto` feature, a signature verifier leveraging the [`rc_crypto` crate](https://github.com/mozilla/application-services/tree/v93.2.0/components/support/rc_crypto).
 /// ```rust
 /// # #[cfg(feature = "rc_crypto_verifier")] {
 /// # use remote_settings_client::Client;
