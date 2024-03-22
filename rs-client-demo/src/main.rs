e837036a11576f30ea019dc007c7846466be8beb
Mathieu Leplatre <mathieu@mozilla.com>
2024-03-22 15:08:35 +0100

    Specify server via ENV env var

--- rs-client-demo/src/main.rs
+++ rs-client-demo/src/main.rs
@@ -3,7 +3,9 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 use remote_settings_client::client::net::ViaductClient;
-use remote_settings_client::client::{FileStorage, RingVerifier};
+use remote_settings_client::client::{
+    FileStorage, RingVerifier, DEFAULT_SERVER_URL, PROD_CERT_ROOT_HASH,
+};
 use remote_settings_client::{Client, Record};
 use serde::Deserialize;
 use serde_json::json;
@@ -26,12 +28,13 @@ pub struct LatestChangeEntry {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let server_url = option_env!("SERVER_URL").unwrap_or("http://localhost:8888/v1");
     let authorization = option_env!("AUTHORIZATION").unwrap_or("");
+    let env_name = option_env!("ENV").unwrap_or("prod").to_lowercase();
 
     env_logger::init();
     set_backend(&ReqwestBackend).unwrap();
 
     if authorization.is_empty() {
-        println!("⚠️  No AUTHORIZATION env var defined, skipping write operations.\n");
+        println!("⚠️  No AUTHORIZATION env var defined, skipping write operations.");
     } else {
         // The `product-integrity` collection is automatically created in CI because
         // we use the `testing.ini` config which will create all signed resources on startup.
@@ -72,12 +75,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         let records = preview_client.get().await?;
         println!("{} record(s) published", records.len());
-
-        println!("\n\n");
     }
 
-    println!("Fetch all Remote Settings collections from DEV server.");
-    let server_url = "https://remote-settings-dev.allizom.org/v1";
+    println!("\n\n");
+
+    let server_url = match env_name.as_str() {
+        "dev" => "https://remote-settings-dev.allizom.org/v1",
+        "stage" => "https://remote-settings.allizom.org/v1",
+        _ => DEFAULT_SERVER_URL,
+    };
+    let root_hash = match env_name.as_str() {
+        "dev" => "45:C3:7F:3A:09:A6:D7:0E:0F:A3:21:FB:29:75:3B:A7:99:8F:12:59:B3:27:72:76:8F:23:CC:DC:24:83:67:98",
+        "stage" => "3C:01:44:6A:BE:90:36:CE:A9:A0:9A:CA:A3:A5:20:AC:62:8F:20:A7:AE:32:CE:86:1C:B2:EF:B7:0F:A0:C7:45",
+        _ => PROD_CERT_ROOT_HASH,
+    };
+
+    println!(
+        "Fetch all Remote Settings collections from {} server.",
+        env_name.to_uppercase()
+    );
     let poll_url = format!(
         "{}/buckets/monitor/collections/changes/changeset?_expected=0",
         server_url
@@ -111,8 +127,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             .http_client(Box::new(ViaductClient))
             .bucket_name(&collection.bucket)
             .collection_name(&collection.collection)
-            .cert_root_hash("45:C3:7F:3A:09:A6:D7:0E:0F:A3:21:FB:29:75:3B:A7:99:8F:12:59:B3:27:72:76:8F:23:CC:DC:24:83:67:98".to_owned())
-            .signer_name(format!("cas_cur_{}.content-signature.mozilla.org", signer_name))
+            .cert_root_hash(root_hash.to_owned())
+            .signer_name(format!("{}.content-signature.mozilla.org", signer_name))
             .storage(Box::new(FileStorage {
                 folder: temp_dir,
                 ..FileStorage::default()
