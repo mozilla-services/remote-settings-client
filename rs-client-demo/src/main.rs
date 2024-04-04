237e03d09b5fa253ba75ea8012f08901a11e43a5
Mathieu Leplatre <mathieu@mozilla.com>
2024-04-04 17:12:32 +0200

    Pass `ENV` var to run demo against DEV or STAGE (#413)
    * Use DEV server in demo
    
    * Skip write operations if no AUTHORIZATION env var defined
    
    * Use flavor as suggested by @willdurand
    
    * Specify server via ENV env var
    
    * Remove pinning collections
    
    * Use new root hash on STAGE

--- rs-client-demo/src/main.rs
+++ rs-client-demo/src/main.rs
@@ -3,7 +3,9 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 use remote_settings_client::client::net::ViaductClient;
-use remote_settings_client::client::{FileStorage, RingVerifier};
+use remote_settings_client::client::{
+    FileStorage, RingVerifier, DEFAULT_SERVER_URL, PROD_CERT_ROOT_HASH,
+};
 use remote_settings_client::{Client, Record};
 use serde::Deserialize;
 use serde_json::json;
@@ -22,71 +24,92 @@ pub struct LatestChangeEntry {
     pub collection: String,
 }
 
-#[tokio::main]
+#[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let server_url = option_env!("SERVER_URL").unwrap_or("http://localhost:8888/v1");
     let authorization = option_env!("AUTHORIZATION").unwrap_or("");
-    // The `product-integrity` collection is automatically created in CI because
-    // we use the `testing.ini` config which will create all signed resources on startup.
-    // See `KINTO_SIGNER_RESOURCES` in `.circleci/config.yml`.
-    let collection = option_env!("COLLECTION").unwrap_or("product-integrity");
+    let env_name = option_env!("ENV").unwrap_or("prod").to_lowercase();
 
     env_logger::init();
     set_backend(&ReqwestBackend).unwrap();
 
-    println!("Connect to local server {}", server_url);
-    let editor_client = Client::builder()
-        .http_client(Box::new(ViaductClient))
-        .server_url(server_url)
-        .authorization(authorization)
-        .collection_name(collection)
-        .build()
-        .unwrap();
+    if authorization.is_empty() {
+        println!("⚠️  No AUTHORIZATION env var defined, skipping write operations.");
+    } else {
+        // The `product-integrity` collection is automatically created in CI because
+        // we use the `testing.ini` config which will create all signed resources on startup.
+        // See `KINTO_SIGNER_RESOURCES` in `.circleci/config.yml`.
+        let collection = option_env!("COLLECTION").unwrap_or("product-integrity");
 
-    println!("main-workspace/{}: Create a record", collection);
-    editor_client
-        .store_record(Record::new(json!({
-          "id": "my-key",
-          "foo": "bar"
-        })))
-        .await?;
+        println!("Connect to local server {}", server_url);
+        let editor_client = Client::builder()
+            .http_client(Box::new(ViaductClient))
+            .server_url(server_url)
+            .authorization(authorization)
+            .collection_name(collection)
+            .build()
+            .unwrap();
 
-    println!("main-workspace/{}: Request review from peers", collection);
-    editor_client.request_review("I made changes").await?;
+        println!("main-workspace/{}: Create a record", collection);
+        editor_client
+            .store_record(Record::new(json!({
+            "id": "my-key",
+            "foo": "bar"
+            })))
+            .await?;
 
-    print!(
-        "main-workspace/{}: Fetching preview records with default `Verifier`...",
-        collection
-    );
-    let mut preview_client = Client::builder()
-        .http_client(Box::new(ViaductClient))
-        .server_url(server_url)
-        .bucket_name("main-preview")
-        .collection_name(collection)
-        .build()
-        .unwrap();
+        println!("main-workspace/{}: Request review from peers", collection);
+        editor_client.request_review("I made changes").await?;
 
-    let records = preview_client.get().await?;
-    println!("{} record(s) published", records.len());
+        print!(
+            "main-workspace/{}: Fetching preview records with default `Verifier`...",
+            collection
+        );
+        let mut preview_client = Client::builder()
+            .http_client(Box::new(ViaductClient))
+            .server_url(server_url)
+            .bucket_name("main-preview")
+            .collection_name(collection)
+            .build()
+            .unwrap();
+
+        let records = preview_client.get().await?;
+        println!("{} record(s) published", records.len());
+    }
 
     println!("\n\n");
 
-    println!("Fetch all Remote Settings collections from PROD server.");
-    let url = "https://firefox.settings.services.mozilla.com/v1/buckets/monitor/collections/changes/changeset?_expected=0";
-    let response =
-        tokio::task::spawn_blocking(move || Request::get(Url::parse(url).unwrap()).send().unwrap())
-            .await
-            .unwrap();
-    let collections: KintoPluralResponse<LatestChangeEntry> = response.json().unwrap();
+    let server_url = match env_name.as_str() {
+        "dev" => "https://remote-settings-dev.allizom.org/v1",
+        "stage" => "https://remote-settings.allizom.org/v1",
+        _ => DEFAULT_SERVER_URL,
+    };
+    let root_hash = match env_name.as_str() {
+        "dev" => "45:C3:7F:3A:09:A6:D7:0E:0F:A3:21:FB:29:75:3B:A7:99:8F:12:59:B3:27:72:76:8F:23:CC:DC:24:83:67:98",
+        "stage" => "45:C3:7F:3A:09:A6:D7:0E:0F:A3:21:FB:29:75:3B:A7:99:8F:12:59:B3:27:72:76:8F:23:CC:DC:24:83:67:98",
+        _ => PROD_CERT_ROOT_HASH,
+    };
 
+    println!(
+        "Fetch all Remote Settings collections from {} server.",
+        env_name.to_uppercase()
+    );
+    let poll_url = format!(
+        "{}/buckets/monitor/collections/changes/changeset?_expected=0",
+        server_url
+    );
+    let response = tokio::task::spawn_blocking(move || {
+        Request::get(Url::parse(&poll_url).unwrap()).send().unwrap()
+    })
+    .await
+    .unwrap();
+    let collections: KintoPluralResponse<LatestChangeEntry> = response.json().unwrap();
     for collection in &collections.changes {
         let cid = format!("{}/{}", collection.bucket, collection.collection);
         print!("{}: ", cid);
 
         // We use different signing chains depending on the bucket/collection.
         let signer_name = match collection.bucket.as_str() {
-            "pinning" => "pinning-preload",
-            "pinning-preview" => "pinning-preload",
             "security-state" => "onecrl",
             "security-state-preview" => "onecrl",
             "blocklists" => match collection.collection.as_str() {
@@ -98,9 +121,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         let temp_dir = std::env::temp_dir();
         let mut client = Client::builder()
+            .server_url(server_url)
             .http_client(Box::new(ViaductClient))
             .bucket_name(&collection.bucket)
             .collection_name(&collection.collection)
+            .cert_root_hash(root_hash.to_owned())
             .signer_name(format!("{}.content-signature.mozilla.org", signer_name))
             .storage(Box::new(FileStorage {
                 folder: temp_dir,
@@ -110,8 +135,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             .build()
             .unwrap();
 
-        let records = client.get().await?;
-        println!("Found {} records", records.len());
+        match client.get().await {
+            Ok(records) => println!("{} records ✅.", records.len()),
+            Err(err) => println!("{} ❌", err),
+        }
     }
 
     Ok(())
